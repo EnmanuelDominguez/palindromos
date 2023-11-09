9779da0fb65cfa2fcd097e43244eee240c9d17e6
EnmanuelDominguez <denmanuel795@gmail.com>
2023-11-09 01:25:30 -0500

    Subiendo logica para doPut y doDelete

--- src/main/java/com/elpoli/edu/co/ces3/universitas/Servlets/StudentServlet.java
+++ src/main/java/com/elpoli/edu/co/ces3/universitas/Servlets/StudentServlet.java
@@ -78,10 +78,63 @@ public class StudentServlet extends MyServlet {
     }
 
     public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        JsonObject body = this.getParamsFromPost(request);
+
+        int studentId = body.get("id").getAsInt();
+
+        Student studentToUpdate = null;
+        for (Student student : students) {
+            if (student.getId() == studentId) {
+                studentToUpdate = student;
+                break;
+            }
+        }
+
+        if (studentToUpdate != null) {
+            studentToUpdate.setNombre(body.get("nombre").getAsString());
+            studentToUpdate.setCedula(body.get("cedula").getAsString());
+
+            PrintWriter out = response.getWriter();
+            out.println("Se actualizo la informacion.");
+            out.flush();
+            System.out.println("El id " + studentId + " se actualizo");
+
+        } else {
+            PrintWriter out = response.getWriter();
+            out.println("No se encontro informacion.");
+            out.flush();
+        }
     }
 
     public void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+
+        String studentId = request.getParameter("studentId");
+        if (studentId != null) {
+            int idToDelete = Integer.parseInt(studentId);
+            Student studentToDelete = null;
+
+            for (Student student : students) {
+                if (student.getId() == idToDelete) {
+                    studentToDelete = student;
+                    break;
+                }
+            }
+
+            if (studentToDelete != null) {
+                students.remove(studentToDelete);
+                PrintWriter out = response.getWriter();
+                out.println("El id " + idToDelete + " se elimino.");
+                out.flush();
+            } else {
+                PrintWriter out = response.getWriter();
+                out.println("No se encontro información");
+            }
+        }
+        System.out.println("Se elimino el id " + studentId);
     }
+
         public void destroy() {
     }
 }
