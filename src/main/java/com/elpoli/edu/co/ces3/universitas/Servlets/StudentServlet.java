4e0e1bfa8b5d0b267a03ae46674004f1acb79107
EnmanuelDominguez <denmanuel795@gmail.com>
2023-11-09 01:04:17 -0500

    Cambios en POM añadiendo dependencia, y doGet para consultar estudiantes en hardcode

--- src/main/java/com/elpoli/edu/co/ces3/universitas/Servlets/StudentServlet.java
+++ src/main/java/com/elpoli/edu/co/ces3/universitas/Servlets/StudentServlet.java
@@ -1,28 +1,63 @@
 package com.elpoli.edu.co.ces3.universitas.Servlets;
 
+import com.elpoli.edu.co.ces3.universitas.Objects.Student;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 @WebServlet(name = "studentServlet", value = "/student")
 public class StudentServlet extends HttpServlet {
     private String message;
-
+    private GsonBuilder gsonBuilder;
+    private Gson gson;
+    private ArrayList<Student> students;
     public void init() {
-        message = "Hello World!";
+
+        gsonBuilder = new GsonBuilder();
+        gson = gsonBuilder.create();
+
+        students = new ArrayList<>();
+        Student student1 = new Student();
+        student1.id = 10;
+        student1.setNombre("Enmanuel");
+        student1.setCedula("1140423482");
+        students.add(student1);
+
+        for (int i = 0; i < students.size(); i = i+1){
+            System.out.println(students.get(i));
+        }
+
+        message = "Hello student.";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        response.setContentType("application/json");
 
-        // Hello
+        //getStudentByParam
+        String studentId = request.getParameter("studentId");
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        //Si param viene vacio...
+        if (studentId == null){
+            out.println(gson.toJson(students));
+            //Si param viene lleno...
+        } else {
+            Student studentSearch = null;
+            //For mejorado realizado en clase
+            for (Student s: students){
+                if (s.getId() == Integer.parseInt(studentId)){
+                    studentSearch = s;
+                    break;
+                }
+            }
+            out.println(gson.toJson(studentSearch));
+        }
     }
 
     public void destroy() {
